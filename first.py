72ae2cbd33e62663b308697f56a78b3fc00ce980
GalaxyFFs <122290888+GalaxyFFs@users.noreply.github.com>
2025-10-04 19:20:10 +0000

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+def sudy_nebo_lichy(cislo):
+    if cislo % 2 == 0:
+        return(f"Číslo {cislo} je sudé")
+    else:
+        return(f"Číslo {cislo} je liché")
+    
+if __name__ == "__main__":
+    print(sudy_nebo_lichy(5))
+    print(sudy_nebo_lichy(1000000))
